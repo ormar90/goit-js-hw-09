cfd4d86295fa3ffdb8026772f31a7ae7cb974d56
Orest Maryniuk <98083499+ormar90@users.noreply.github.com>
2022-08-13 21:47:24 +0300

    finished task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body')
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStartEl.addEventListener('click', onClickStart);
+buttonStopEl.addEventListener('click', onClickStop);
+
+function onClickStart(e) {
+    bodyEl.style.backgroundColor = getRandomHexColor();    
+
+    intervalId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+    buttonStartEl.disabled = true;
+    buttonStopEl.disabled = false;
+}
+
+function onClickStop(e) {
+    clearInterval(intervalId);
+
+    buttonStopEl.disabled = true;
+    buttonStartEl.disabled = false;
+}
